a69d04bc396154ef353faa2e2da17855c458fd74
leviet <levietviet44266@gmail.com>
2024-12-17 16:44:01 +0700

    trend side bar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
+import TrendSidebar from "@/components/TrendSidebar";
 import prisma from "@/lib/prisma";
 import { postDataInclude } from "@/lib/type";
 
@@ -9,13 +10,14 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <main className="w-full">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {post.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendSidebar />
     </main>
   );
 }
